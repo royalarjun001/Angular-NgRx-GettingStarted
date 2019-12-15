ff32570620113e80ec61e01cc904242dd63495dd
Arjun kumar verma <arjun.kumar.verma@jci.com>
2019-12-15 09:04:40 +0530

    Subscribing to the select for the store update

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'pm-product-list',
@@ -34,6 +34,12 @@ export class ProductListComponent implements OnInit, OnDestroy {
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => this.products = products,
       error: (err: any) => this.errorMessage = err.error
+    });
+
+    this.store.pipe(select('product')).subscribe(product => {
+      if (product) {
+        this.displayCode = product.showProductCode;
+      }
     });
   }
 
